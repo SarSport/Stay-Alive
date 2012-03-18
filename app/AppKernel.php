65269cb63e3ec60dd7b0985d89abab91f3b67ca9
Dmitry Petrov <old.fightmaster@gmail.com>
2012-03-18 15:06:56 +0400

    Added empty ApplicationBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -27,7 +27,8 @@ class AppKernel extends Kernel
             new FOS\UserBundle\FOSUserBundle(),
             new JMS\SerializerBundle\JMSSerializerBundle($this),
             new Escape\WSSEAuthenticationBundle\EscapeWSSEAuthenticationBundle(),
-
+,
+            new SarSport\Bundle\ApplicationBundle\SarSportApplicationBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/SarSport/Bundle/ApplicationBundle/SarSportApplicationBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace SarSport\Bundle\ApplicationBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class SarSportApplicationBundle extends Bundle
+{
+}
